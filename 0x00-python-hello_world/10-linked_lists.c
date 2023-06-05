1eb7321b0d4c21967282d46548b8a6c803978815
michaeljumah <michaeljumah8@gmail.com>
2023-06-05 10:32:24 -0700

    10-linked_lists.c

--- 0x00-python-hello_world/10-linked_lists.c
+++ 0x00-python-hello_world/10-linked_lists.c
@@ -10,7 +10,7 @@
 size_t print_listint(const listint_t *h)
 {
     const listint_t *current;
-    unsigned int n; /* number of nodes */
+    unsigned int a; /* number of nodes */
 
     current = h;
     a = 0;
