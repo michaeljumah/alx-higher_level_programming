d7fd2f7d6102550500e7513faf955e93eef67c59
michaeljumah <michaeljumah8@gmail.com>
2023-06-06 10:07:57 -0700

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new_string = ""
+    for i in range(len(str)):
+        if (ord(str[i]) >= 97 and ord(str[i]) <= 122):
+            new_string += chr(ord(str[i]) - 32)
+            continue
+        new_string += str[i]
+
+    print('{0}'.format(new_string))
+
