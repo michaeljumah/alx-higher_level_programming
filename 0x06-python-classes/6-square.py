2ed5c9a2d14f6592ae2428cfb28b896dc15a3f93
michaeljumah <michaeljumah8@gmail.com>
2023-06-27 07:44:53 -0700

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -41,12 +41,14 @@ class Square:
         """Return the current area of the square"""
         return self.__size * self.__size
 
-    def myprint(self):
+    def my_print(self):
         """Print the square with the # char"""
         if self.__size == 0:
-            print()
-        else:
-            for _ in range(self.__position[1]):
-                print()
-            for _ in range(self.__size):
-                print(""* self.__position[0] + *self.size)
+            print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
