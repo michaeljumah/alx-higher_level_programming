c0f2b22603ba8090a9e50e7e79d9b8ac1f06aec3
michaeljumah <michaeljumah8@gmail.com>
2023-06-27 07:40:22 -0700

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Define a class square."""
 
+"""Define a class square."""
 
 class Square(object):
     """class variable size"""
@@ -44,11 +44,9 @@ class Square(object):
     def myprint(self):
         """Print the square with the # char"""
         if self.__size == 0:
-            print("")
-            return
-
-        [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            print("")
+            print()
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(""* self.__position[0] + *self.size)
