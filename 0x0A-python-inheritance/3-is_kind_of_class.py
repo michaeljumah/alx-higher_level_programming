7c48aeed39f5efca9d8aa6c9c61c1159a9cbf88f
michaeljumah <michaeljumah8@gmail.com>
2023-07-10 08:20:51 -0700

    3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a class and inherited class_checking function."""
+"""Defines a class adn inherited class_checking function."""
 
 def is_kind_of_class(obj, a_class):
     """
