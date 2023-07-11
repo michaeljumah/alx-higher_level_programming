7ad4d024c8da0bb8abba5fe667c85056687200fc
michaeljumah <michaeljumah8@gmail.com>
2023-07-11 09:27:40 -0700

    0-read_file.py git push "

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that reads a text file"""
+
+
+def read_file(filename=""):
+    """line:returns line"""
+    with open('filename',encoding='utf-8') as f:
+        for line in f:
+            print(line, end='')
