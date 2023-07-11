3e5612506435ee7a6f81de5425cb192d0b1a3bda
michaeljumah <michaeljumah8@gmail.com>
2023-07-11 09:39:14 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,4 @@
 def read_file(filename=""):
     """line:returns line"""
     with open('filename',encoding='utf-8') as f:
-        for line in f:
-            print(line, end='')
+        print(f.read(), end='')
