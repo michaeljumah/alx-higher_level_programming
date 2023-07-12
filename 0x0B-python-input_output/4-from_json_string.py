5423b53bb343ae81c1664f34a843b32fddd90046
michaeljumah <michaeljumah8@gmail.com>
2023-07-12 08:39:36 -0700

    4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -5,6 +5,5 @@ import json
 
 
 def from_json_string(my_str):
-    """returns an object represented by a json string
-    """
+    """Returns an object represented by a json string"""
     return json.loads(my_str)
