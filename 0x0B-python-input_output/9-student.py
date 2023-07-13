29ddcbc8ddde4ee2ff2da8f399cf7cb623f41de8
michaeljumah <michaeljumah8@gmail.com>
2023-07-13 04:21:09 -0700

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
-""" """
+"""Defines a student class """
 
 
 class Student:
+    """represents a student """
     def __init__(self, first_name, last_name, age):
+        """__int__:initializes a constructor
+        Args:
+            self.first_name:first name
+            self.last_name: last name
+            self.age: age
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """gets a dictionary representation of a student """
         return self.__dict__
