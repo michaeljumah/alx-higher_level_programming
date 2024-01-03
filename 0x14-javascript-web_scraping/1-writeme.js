61fc648df245d64589f801d772c7f0ecbe9984cd
michaeljumah <michaeljumah8@gmail.com>
2024-01-03 20:19:07 +0000

    correction

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (error) {
   if (error) {
     console.log(error);
   }
